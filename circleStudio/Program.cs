b8a701d71eb50b0cb5f54f8a7d3fee70706abc34
Tasha L <natasha.lewis.lalor@gmail.com>
2022-08-03 20:15:19 -0500

    Finished! lol

--- circleStudio/Program.cs
+++ circleStudio/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a radius in miles: ");
+            int radius = int.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
+            double circumference = 2 * Math.PI * radius;
+            double diameter = 2 * radius;
+            Console.WriteLine($"The area is {area}. The circumference is {circumference}. Diameter is {diameter}");
+
+            Console.Write("How many miles per gallon does your car get? ");
+            double mpg = double.Parse(Console.ReadLine());
+            Console.WriteLine($"It will take {circumference / mpg} gallons to get around that dang circle");
         }
-    }
-}
+    } 
+} 
